d5ca47b9b5292463ef7768efc671180df44d5fc6
Thanh BUI <bryonbui@gmail.com>
2022-06-08 13:10:15 +0200

    More unit tests for statement printing

--- src/test/java/kata/bank/StatementShould.java
+++ src/test/java/kata/bank/StatementShould.java
@@ -1,6 +1,10 @@
 package kata.bank;
 
+import static java.util.Arrays.asList;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import java.util.Collections;
 import org.junit.jupiter.api.Test;
@@ -11,17 +15,33 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 public class StatementShould {
+  @Mock
+  private Console console;
+
+  @Mock
+  private TransactionFormatter formatter;
+
+  @Mock
+  private Transaction transaction1, transaction2;
 
   @InjectMocks
   private Statement statement;
 
-  @Mock
-  private Console console;
+
 
   @Test
   public void print_an_empty_statement() {
     statement.printStatement(Collections.emptyList());
+    verify(formatter, never()).format(any());
     verify(console).print("Operation || Date || Amount || Balance");
+  }
+
+  @Test
+  public void print_a_formatted_transaction() {
+    when(formatter.format(transaction1)).thenReturn("some formatted transaction");
+    statement.printStatement(asList(transaction1));
+    verify(console).print("Operation || Date || Amount || Balance\n" +
+        "some formatted transaction");
   }
 
 }
