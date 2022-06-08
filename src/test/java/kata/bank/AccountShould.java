17846e2e84669dcfa174b204d204a694cae3e422
Thanh BUI <bryonbui@gmail.com>
2022-06-08 12:59:22 +0200

    Add more unit test to Account + refactoring

--- src/test/java/kata/bank/AccountShould.java
+++ src/test/java/kata/bank/AccountShould.java
@@ -5,9 +5,11 @@ import static org.mockito.Mockito.when;
 import static org.mockito.Mockito.verify;
 
 import java.time.LocalDateTime;
+import java.util.Collections;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
@@ -20,13 +22,14 @@ public class AccountShould {
   Clock clock;
   @Mock
   Statement statement;
+  @InjectMocks
+  Account account;
 
   @Test
   public void post_deposit() {
     int amount = 400;
     LocalDateTime timestamp = LocalDateTime.now();
     when(clock.currentTime()).thenReturn(timestamp);
-    Account account = new Account(transactionRepo, clock, statement);
     account.deposit(amount);
 
     Transaction depositTransaction = new Transaction(amount, timestamp, TransactionType.CREDIT,
@@ -39,7 +42,6 @@ public class AccountShould {
     int amount = 200;
     LocalDateTime timestamp = LocalDateTime.now();
     when(clock.currentTime()).thenReturn(timestamp);
-    Account account = new Account(transactionRepo, clock, statement);
     account.withdraw(amount);
 
     Transaction withdrawalTransaction = new Transaction(amount, timestamp, TransactionType.DEBIT,
@@ -49,12 +51,30 @@ public class AccountShould {
 
   @Test
   public void print_a_statement() {
-    Account account = new Account(transactionRepo, clock, statement);
     List<Transaction> transactions = asList(createTransaction(500, TransactionType.CREDIT),
         createTransaction(100, TransactionType.DEBIT));
     when(transactionRepo.findAllTransactions()).thenReturn(transactions);
     account.printStatement();
     verify(statement).printStatement(transactions);
+  }
+
+  @Test
+  public void calculate_a_running_balance() {
+    LocalDateTime timestamp = LocalDateTime.now();
+    when(clock.currentTime()).thenReturn(timestamp);
+
+    Transaction transaction1 = new Transaction(100, timestamp, TransactionType.CREDIT, 100);
+    Transaction transaction2 = new Transaction(75, timestamp, TransactionType.DEBIT, 25);
+
+    when(transactionRepo.findAllTransactions())
+        .thenReturn(Collections.emptyList())
+        .thenReturn(asList(transaction1));
+
+    account.deposit(100);
+    account.withdraw(75);
+
+    verify(transactionRepo).postTransaction(transaction1);
+    verify(transactionRepo).postTransaction(transaction2);
   }
 
   private Transaction createTransaction(int amount, TransactionType type) {
