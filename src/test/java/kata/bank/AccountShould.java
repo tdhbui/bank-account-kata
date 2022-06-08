2261f6651e9e399f3cf2b535fe73fae0ea1e242c
Thanh BUI <bryonbui@gmail.com>
2022-06-08 10:53:19 +0200

    Unit test for deposit operation in account

---
+++ src/test/java/kata/bank/AccountShould.java
@@ -0,0 +1,31 @@
+package kata.bank;
+
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.verify;
+
+import java.time.LocalDateTime;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class AccountShould {
+
+  @Mock
+  TransactionRepo transactionRepo;
+  @Mock
+  Clock clock;
+
+  @Test
+  public void post_deposit() {
+    int amount = 400;
+    LocalDateTime timestamp = LocalDateTime.now();
+    when(clock.currentTime()).thenReturn(timestamp);
+    Account account = new Account(transactionRepo, clock);
+    account.deposit(amount);
+
+    Transaction depositTransaction = new Transaction(amount, timestamp, TransactionType.CREDIT);
+    verify(transactionRepo).postTransaction(depositTransaction);
+  }
+}
